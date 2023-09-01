9869d4aa4c69d6f26043834d49b99d2ca86b5589
vishal-carvia <vishal.carvia@gmail.com>
2023-09-01 18:16:01 +0530

    Initial commit

---
+++ lib/demo_buttons.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class DemoButtons extends StatefulWidget {
+  const DemoButtons({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _DemoButtonsState();
+  }
+}
+
+class _DemoButtonsState extends State<DemoButtons> {
+  @override
+  Widget build(BuildContext context) {
+    var _isUnderstood = false;
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _isUnderstood = false;
+                    });
+                  },
+                  child: const Text('No'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _isUnderstood = true;
+                    });
+                  },
+                  child: const Text('Yes'),
+                ),
+              ],
+            ),
+            if (_isUnderstood) const Text('Awesome!'),
+          ],
+        )
+      ],
+    );
+  }
+}
---
+++ lib/ui_updates_demo.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class UIUpdatesDemo extends StatefulWidget {
+  const UIUpdatesDemo({super.key});
+
+  @override
+  StatefulElement createElement() {
+    print('UIUpdatesDemo CREATEELEMENT called');
+    return super.createElement();
+  }
+
+  @override
+  State<UIUpdatesDemo> createState() {
+    return _UIUpdatesDemo();
+  }
+}
+
+class _UIUpdatesDemo extends State<UIUpdatesDemo> {
+  var _isUnderstood = false;
+
+  @override
+  Widget build(BuildContext context) {
+    print('UIUpdatesDemo BUILD called');
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Every Flutter developer should have a basic understanding of Flutter\'s internals!',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'Do you understand how Flutter updates UIs?',
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 24),
+          ],
+        ),
+      ),
+    );
+  }
+}
